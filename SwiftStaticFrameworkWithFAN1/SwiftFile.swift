9e78e0bdc6922145006f73af55efaf2fe95eb780
Bryan Boyko <bryan@maxprebid.com>
2018-05-01 16:21:20 -0700

    Add a swift file without FAN imported. Recreate static framework (only simulator architecture this time)

---
+++ SwiftStaticFrameworkWithFAN1/SwiftFile.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftFile.swift
+//  SwiftStaticFrameworkWithFAN1
+//
+//  Created by Bryan Boyko on 5/1/18.
+//  Copyright © 2018 BB. All rights reserved.
+//
+
+import Foundation
+
+public class SwiftFile {
+    
+    public init() {
+        print("SwiftFile initialized")
+    }
+    
+    public func doSomething() {
+        print("SwiftFile did something")
+    }
+}
